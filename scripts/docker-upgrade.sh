afae7cb3494a06e6da87f4a829af4f72e314d839
trueai-org <circle@trueai.org>
2026-03-24 00:23:17 +0800

    sh

--- scripts/docker-upgrade.sh
+++ scripts/docker-upgrade.sh
@@ -4,49 +4,17 @@
 IMAGE_NAME="registry.cn-guangzhou.aliyuncs.com/trueai-org/docker-proxy"
 CONTAINER_NAME="docker-proxy"
 
-# 打印信息
 echo "开始更新 ${CONTAINER_NAME} 容器..."
 
-# 验证Docker是否安装
-if ! command -v docker &> /dev/null && ! command -v docker.io &> /dev/null
-then
-    # 再尝试查找 docker 可执行文件
-    if [ ! -x /usr/bin/docker ] && [ ! -x /usr/bin/docker.io ]
-    then
-        echo "Docker 未安装，请先安装 Docker。"
-        exit 1
-    fi
-fi
-
-# 如果脚本运行到这里，说明找到了 Docker
-echo "Docker 已安装，继续执行..."
+# 创建网络（已存在则忽略）
+docker network create ${NETWORK_NAME} 2>/dev/null || true
 
 # 拉取最新镜像
 echo "拉取最新的镜像 ${IMAGE_NAME}..."
-docker pull ${IMAGE_NAME}
-if [ $? -ne 0 ]; then
-    echo "拉取镜像失败，请检查网络连接或镜像地址是否正确。"
-    exit 1
-fi
+docker pull ${IMAGE_NAME} || { echo "拉取镜像失败"; exit 1; }
 
-# 停止并移除现有容器
-if [ "$(docker ps -q -f name=${CONTAINER_NAME})" ]; then
-    echo "停止现有的容器 ${CONTAINER_NAME}..."
-    docker stop ${CONTAINER_NAME}
-    if [ $? -ne 0 ]; then
-        echo "停止容器失败，请手动检查。"
-        exit 1
-    fi
-fi
-
-if [ "$(docker ps -aq -f status=exited -f name=${CONTAINER_NAME})" ]; then
-    echo "移除现有的容器 ${CONTAINER_NAME}..."
-    docker rm ${CONTAINER_NAME}
-    if [ $? -ne 0 ]; then
-        echo "移除容器失败，请手动检查。"
-        exit 1
-    fi
-fi
+# 强制停止并移除容器（如果存在）
+docker rm -f ${CONTAINER_NAME} 2>/dev/null || true
 
 # 运行新的容器
 echo "启动新的容器 ${CONTAINER_NAME}..."
@@ -61,16 +29,14 @@ chmod -R 777 ./cache
 chmod -R 777 ./logs
 
 docker run --name ${CONTAINER_NAME} -d --restart=always \
- -p 8080:8080 \
+ -p 8090:8080 \
+ --cpus="1" \
  -v ./logs:/app/logs:rw \
  -v ./cache:/app/cache:rw \
  -v ./appsettings.json:/app/appsettings.json:ro \
  -e TZ=Asia/Shanghai \
- ${IMAGE_NAME}
-if [ $? -ne 0 ]; then
-    echo "启动新的容器失败，请手动检查。"
-    exit 1
-fi
+ --log-opt max-size=10m \
+ --log-opt max-file=10 \
+ ${IMAGE_NAME} || { echo "启动容器失败"; exit 1; }
 
 echo "容器 ${CONTAINER_NAME} 更新并启动成功！"
-
